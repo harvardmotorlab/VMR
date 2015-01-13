3232669d2976b8e7b848cfec4811883cee7198e9
harvardmotorlab <harvardmotorlab@users.noreply.github.com>
2015-01-13 12:59:59 -0500

    Create exprun_strategy_VMR_Exp_a.m
    function exprun_strategy_VMR_Exp_a(name_prefix,c)

---
+++ exprun_strategy_VMR_Exp_a.m
@@ -0,0 +1,22 @@
+function exprun_strategy_VMR_Exp_a(name_prefix,c)
+
+tgt_prefix = [pwd,'\tgt_files_aimingtag_b\vmr_aimingtag_',c];
+% dummy=input('Familiarization 1: ');
+% runexp_strategy_VMR_Exp_a(name_prefix,tgt_prefix,'a')
+% dummy=input('Familiarization 2: ');
+% runexp_strategy_VMR_Exp_a(name_prefix,tgt_prefix,'b')
+% dummy=input('Baseline 1: ');
+% runexp_strategy_VMR_Exp_a(name_prefix,tgt_prefix,'c')
+% % dummy=input('Baseline 2: ');
+% % runexp_strategy_VMR_Exp_a(name_prefix,tgt_prefix,'d')
+dummy=input('Training 1: ');
+runexp_strategy_VMR_Exp_a(name_prefix,tgt_prefix,'f')
+dummy=input('Training 2: ');
+runexp_strategy_VMR_Exp_a(name_prefix,tgt_prefix,'g')
+dummy=input('Training 3: ');
+runexp_strategy_VMR_Exp_a(name_prefix,tgt_prefix,'f')
+dummy=input('Training 4: ');
+runexp_strategy_VMR_Exp_a(name_prefix,tgt_prefix,'h')
+dummy=input('Training 5: ');
+runexp_strategy_VMR_Exp_a(name_prefix,tgt_prefix,'i')
+return
