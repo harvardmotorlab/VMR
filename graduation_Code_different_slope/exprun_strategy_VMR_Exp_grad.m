848b27d9e349ab2c602336605a9c855f7e820f46
harvardmotorlab <motorlab.experiments@gmail.com>
2015-02-11 15:29:41 -0500

    Strategy VMR -graduation_Code_different_slope

---
+++ graduation_Code_different_slope/exprun_strategy_VMR_Exp_grad.m
@@ -0,0 +1,44 @@
+function exprun_strategy_VMR_Exp_grad(name_prefix,c)
+mkdir(name_prefix);
+tgt_prefix = [pwd,'\tgt_files_aimingtag_grad\vmr_aimingtag_',c];
+dummy=input('Familiarization 1: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'a')
+dummy=input('Familiarization 2: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'b')
+dummy=input('Baseline 1: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'c')
+dummy=input('Baseline 2: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'d')
+dummy=input('Baseline 3: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'e')
+dummy=input('Training 1: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'f')
+dummy=input('Training 2: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'g')
+dummy=input('Training 3: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'h')
+dummy=input('Training 4: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'i')
+dummy=input('Training 5: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'j')
+dummy=input('Training 6: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'k')
+dummy=input('Training 7: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'l')
+dummy=input('Training 8: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'m')
+dummy=input('Training 9: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'n')
+dummy=input('Training 10: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'o')
+dummy=input('Training 11: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'p')
+dummy=input('Training 12: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'q')
+dummy=input('Training 13: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'r')
+dummy=input('Training 14: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'s')
+dummy=input('Training 15: ');
+runexp_strategy_VMR_Exp_grad(name_prefix,tgt_prefix,'t')
+return
